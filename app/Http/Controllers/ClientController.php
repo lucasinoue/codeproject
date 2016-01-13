5eaab01377535bcd30e939b9d332e04bb4c75f1d
Lucas Inoue <lucas.inoue.lcs@gmail.com>
2016-01-12 23:42:53 -0200

    primeira entrega

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use CodeProject\Http\Requests;
+use CodeProject\Http\Controllers\Controller;
+use CodeProject\Client;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return \CodeProject\Client::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Client::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Client::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return Client::find($id)->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Client::find($id)->delete();
+    }
+}
